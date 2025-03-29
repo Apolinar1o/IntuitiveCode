7a1e271654821e15cfffee73c29b3e110e141d96
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2025-03-29 15:14:04 -0300

    Download dos arquivos

--- main.py
+++ main.py
@@ -7,33 +7,32 @@ url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedad
 response  = requests.get(url)
 
 if response.status_code == 200:
-    print("Aceeso realizado")
+    print("Acesso realizado")
 else :
     print("Falha no site")
     exit()
 
 
-
 soup = BeautifulSoup(response.text, "html.parser")
 link_achados = []
 
+
 for link in soup.find_all("a", href=True):
-    if "Anexo I." in link["href"].lower() or "Anexo II." in link["href"].lower():
+    if "anexo_i" in link["href"].lower() or "anexo_ii." in link["href"].lower():
+        print("222222222222222")
         link_achados.append((link["href"]))
+
+print(link_achados)
 
 os.makedirs("anexos", exist_ok=True)
 
 for link in link_achados:
-    response = requests.get(pdf_link)
+    resposta = requests.get(link)
+    nomeArquivo = os.path.join("anexos", link.split("/")[-1])
+    with open(nomeArquivo, "wb") as pdf_file:
+        pdf_file.write(response.content)
+    print("Download concluido")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 
