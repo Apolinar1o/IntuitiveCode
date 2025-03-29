15136cd55de0fa319d63ea61700e83cbcc2f381f
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2025-03-29 16:10:24 -0300

    download e compressão

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ link_achados = []
 
 
 for link in soup.find_all("a", href=True):
-    if "anexo_i" in link["href"].lower() or "anexo_ii." in link["href"].lower():
-        print("222222222222222")
+    if ("anexo_i" in link["href"].lower() or "anexo_ii." in link["href"].lower()) and ".pdf" in link["href"]:
         link_achados.append((link["href"]))
 
 print(link_achados)
@@ -27,12 +26,17 @@ print(link_achados)
 os.makedirs("anexos", exist_ok=True)
 
 for link in link_achados:
-    resposta = requests.get(link)
+    resposta = requests.get(link, stream=True)
     nomeArquivo = os.path.join("anexos", link.split("/")[-1])
     with open(nomeArquivo, "wb") as pdf_file:
-        pdf_file.write(response.content)
+        pdf_file.write(resposta.content)
     print("Download concluido")
 
 
+zip_file_name = "anexos_compactados.zip"
+with zipfile.ZipFile(zip_file_name, "w") as zipf:
+    for root, dirs, files in os.walk("anexos"):
+        for file in files:
+            zipf.write(os.path.join(root, file), file)
 
 
