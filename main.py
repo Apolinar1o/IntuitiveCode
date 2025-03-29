7893d490d29f24bb54bf4d81bb242c70508b8987
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2025-03-28 21:10:33 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
