e0f13963a8f468142888167298e0ba758952fd34
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2025-03-28 21:44:13 -0300

    baixando os arquivos

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 
-
-
+import requests
+import zipfile
+import os
+from bs4 import BeautifulSoup
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+response  = requests.get(url)
+
+if response.status_code == 200:
+    print("Aceeso realizado")
+else :
+    print("Falha no site")
+    exit()
+
+
+
+soup = BeautifulSoup(response.text, "html.parser")
+link_achados = []
+
+for link in soup.find_all("a", href=True):
+    if "Anexo I." in link["href"].lower() or "Anexo II." in link["href"].lower():
+        link_achados.append((link["href"]))
+
+os.makedirs("anexos", exist_ok=True)
+
+for link in link_achados:
+    response = requests.get(pdf_link)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
